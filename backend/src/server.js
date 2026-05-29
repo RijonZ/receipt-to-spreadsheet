d7c6164417b0fb53c570c272cf93f6b334b184ad
RijonZ <rijonz29@gmail.com>
2026-05-30 00:54:24 +0200

    fix catch-all route for express 5 compatibility

--- backend/src/server.js
+++ backend/src/server.js
@@ -147,7 +147,7 @@ function toFrontendReceipt(receipt) {
 
 const distPath = path.join(__dirname, "../../frontend/dist");
 app.use(express.static(distPath));
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
